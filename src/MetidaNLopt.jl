766600bb74731108075b42196cf7bbcdae9639d7
PharmCat <v.s.arnautov@yandex.ru>
2021-02-19 16:24:51 +0300

    Metida v0.4

--- src/MetidaNLopt.jl
+++ src/MetidaNLopt.jl
@@ -17,7 +17,8 @@ module MetidaNLopt
         x_tol = 1e-12,
         f_tol = 1e-12,
         hcalck::Bool = false,
-        init = nothing) where T
+        init = nothing,
+        io = stdout) where T
         # Optimization function
         if solver == :nlopt
             optfunc = reml_sweep_β_nlopt
@@ -32,7 +33,7 @@ module MetidaNLopt
         if isa(init, Vector{T}) && length(θ) == length(init)
             copyto!(θ, init)
         else
-            initθ = sqrt(initvar(lmm.mf.data[lmm.mf.f.lhs.sym], lmm.mm.m)[1]/4)
+            initθ = sqrt(initvar(lmm.data.yv, lmm.mm.m)[1]/4)
             θ                      .= initθ
             for i = 1:length(θ)
                 if lmm.covstr.ct[i] == :rho
@@ -46,53 +47,57 @@ module MetidaNLopt
         ############################################################################
         #COBYLA
         opt = NLopt.Opt(:LN_BOBYQA,  thetalength(lmm))
-        #NLopt.ftol_rel!(opt, 1.0e-14)
+        #NLopt.ftol_rel!(opt, f_tol)
         NLopt.ftol_abs!(opt, f_tol)
         #NLopt.xtol_rel!(opt, 1.0e-14)
         NLopt.xtol_abs!(opt, x_tol)
         #opt.lower_bounds = lb::Union{AbstractVector,Real}
         #opt.upper_bounds = ub::Union{AbstractVector,Real}
         #-----------------------------------------------------------------------
-        obj = (x,y) -> optfunc(lmm, varlinkvecapply(x, lmm.covstr.ct))[1]
+        obj = (x,y) -> optfunc(lmm, varlinkvecapply(x, lmm.covstr.ct; rholinkf = rholinkf))[1]
         NLopt.min_objective!(opt, obj)
         #Optimization object
         lmm.result.optim = NLopt.optimize!(opt, θ)
         #Theta (θ) vector
         lmm.result.theta  = varlinkvecapply(lmm.result.optim[2], lmm.covstr.ct)
-        try
-            #Hessian
-            if hcalck
-                lmm.result.h      = ForwardDiff.hessian(x -> reml_sweep_β(lmm, x)[1], lmm.result.theta)
-                qrd = qr(lmm.result.h, Val(true))
-                for i = 1:length(lmm.result.theta)
-                    if abs(qrd.R[i,i]) < 1E-10
-                        if lmm.covstr.ct[qrd.jpvt[i]] == :var
-                            lmmlog!(lmm, verbose, LMMLogMsg(:WARN, "Variation QR.R diagonal value ($(qrd.jpvt[i])) is less than 1e-10."))
-                        elseif lmm.covstr.ct[qrd.jpvt[i]] == :rho
-                            lmmlog!(lmm, verbose, LMMLogMsg(:WARN, "Rho QR.R diagonal value ($(qrd.jpvt[i])) is less than 1e-10."))
-                        end
-                    end
-                end
-            end
-            #-2 LogREML, β, iC
-            lmm.result.reml, lmm.result.beta, iC, θ₃ = optfunc(lmm, lmm.result.theta)
+        lmmlog!(io, lmm, verbose, LMMLogMsg(:INFO, "Resulting θ: "*string(lmm.result.theta)))
+
+        #-2 LogREML, β, iC
+        lmm.result.reml, lmm.result.beta, iC, θ₃ = optfunc(lmm, lmm.result.theta)
+        if !isnan(lmm.result.reml) && !isinf(lmm.result.reml)
             #Variance-vovariance matrix of β
             lmm.result.c            = pinv(iC)
             #SE
             lmm.result.se           = sqrt.(diag(lmm.result.c))
-            #Fit true
-            lmm.result.fit          = true
-        catch
-            #-2 LogREML, β, iC
-            lmm.result.reml, lmm.result.beta, iC, θ₃ = optfunc(lmm, lmm.result.theta)
-            #Fit false
-            lmm.result.fit          = false
+            lmmlog!(io, lmm, verbose, LMMLogMsg(:INFO, "Model fitted."))
+            lmm.result.fit      = true
+        else
+            lmmlog!(io, lmm, verbose, LMMLogMsg(:INFO, "Model NOT fitted."))
+            lmm.result.fit      = false
+        end
+        if hcalck && lmm.result.fit
+                #Hessian
+            lmm.result.h      = hessian(lmm, lmm.result.theta)
+                #H positive definite check
+            if !isposdef(Symmetric(lmm.result.h))
+                lmmlog!(io, lmm, verbose, LMMLogMsg(:WARN, "Hessian is not positive definite."))
+            end
+            qrd = qr(lmm.result.h, Val(true))
+            for i = 1:length(lmm.result.theta)
+                if abs(qrd.R[i,i]) < 1E-8
+                    if lmm.covstr.ct[qrd.jpvt[i]] == :var
+                        lmmlog!(io, lmm, verbose, LMMLogMsg(:WARN, "Hessian parameter (variation) QR.R diagonal value ($(qrd.jpvt[i])) is less than 1e-10."))
+                    elseif lmm.covstr.ct[qrd.jpvt[i]] == :rho
+                        lmmlog!(io, lmm, verbose, LMMLogMsg(:WARN, "Hessian parameter (rho) QR.R diagonal value ($(qrd.jpvt[i])) is less than 1e-10."))
+                    end
+                end
+            end
         end
         lmm
     end
 
     function reml_sweep_β_nlopt(lmm, θ::Vector{T}) where T
-        n             = length(lmm.data.block)
+        n             = length(lmm.covstr.vcovblock)
         N             = length(lmm.data.yv)
         c             = (N - lmm.rankx)*log(2π)
         #---------------------------------------------------------------------------
@@ -110,18 +115,23 @@ module MetidaNLopt
         qswm          = zero(Int)
         logdetθ₂      = zero(T)
         @inbounds for i = 1:n
-            q    = length(lmm.data.block[i])
+            q    = length(lmm.covstr.vcovblock[i])
             qswm = q + lmm.rankx
             V   = zeros(T, q, q)
-            zgz_base_inc!(V, θ, lmm.covstr, lmm.data.block[i], lmm.covstr.sblock[i])
-            rmat_base_inc!(V, θ[lmm.covstr.tr[end]], lmm.covstr, lmm.data.block[i], lmm.covstr.sblock[i])
+            zgz_base_inc!(V, θ, lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
+            rmat_base_inc!(V, θ[lmm.covstr.tr[end]], lmm.covstr, lmm.covstr.vcovblock[i], lmm.covstr.sblock[i])
             #-------------------------------------------------------------------
-            X[i] = view(lmm.data.xv,  lmm.data.block[i], :)
-            y[i] = view(lmm.data.yv, lmm.data.block[i])
+            X[i] = view(lmm.data.xv,  lmm.covstr.vcovblock[i], :)
+            y[i] = view(lmm.data.yv, lmm.covstr.vcovblock[i])
             #-------------------------------------------------------------------
             #Cholesky
             A[i] = LinearAlgebra.LAPACK.potrf!('L', V)[1]
-            θ₁  += logdet(Cholesky(A[i], 'L', 0))
+            try
+                θ₁  += logdet(Cholesky(A[i], 'L', 0))
+            catch
+                lmmlog!(lmm, LMMLogMsg(:ERROR, "θ₁ not estimated during REML calculation, V isn't positive definite or |V| less zero."))
+                return (Inf, nothing, nothing, Inf)
+            end
             vX   = LinearAlgebra.LAPACK.potrs!('L', A[i], copy(X[i]))
             vy   = LinearAlgebra.LAPACK.potrs!('L', A[i], copy(y[i]))
             LinearAlgebra.BLAS.gemm!('T', 'N', one(T), X[i], vX, one(T), θ₂tc)
